bd7791a6793ac954eee902c7a702b054d9cbb10d
InsanityBringer <1504999+InsanityBringer@users.noreply.github.com>
2019-02-11 15:43:47 -0600

    you're using VCS now, act like it.

--- app/src/main/java/com/insanitybringer/padsim/game/data/ElementReader.java
+++ app/src/main/java/com/insanitybringer/padsim/game/data/ElementReader.java
@@ -1,27 +1,19 @@
 package com.insanitybringer.padsim.game.data;
 
 //import org.json.*;
-import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
-import com.google.gson.JsonObject;
-
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.BufferedReader;
 import java.io.IOException;
 import com.insanitybringer.padsim.game.Monster;
 import com.insanitybringer.padsim.game.Skill;
 
-import java.nio.ByteBuffer;
 import java.util.List;
 
 import android.app.Application;
 import android.content.res.AssetManager;
-import android.content.res.Resources;
-
-import org.json.JSONArray;
 
 public class ElementReader
 {
@@ -32,83 +24,6 @@ public class ElementReader
     {
         this.application = application;
     }
-    /*
-    //This is not a general purpose reader and has some serious flaws...
-    public void LoadMonsters(List<Monster> monsters)
-    {
-        AssetManager assetManager = application.getResources().getAssets();
-        StringBuilder stringBuilder = new StringBuilder();
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(assetManager.open("pad_na_cards.json"))))
-        {
-            String line = reader.readLine();
-            String json;
-            JSONTokener tokener;
-            JSONObject mons;
-            int baseID;
-            while (line != null)
-            {
-                if (line.charAt(0) != '[')
-                {
-                    if (line.contains("}")) //end of object
-                    {
-                        stringBuilder.append("}\n"); //append a different ending line
-                        json = stringBuilder.toString();
-                        tokener = new JSONTokener(json);
-
-                        Object obj = tokener.nextValue();
-                        if (obj instanceof JSONObject)
-                        {
-                            mons = (JSONObject) obj;
-                            Monster monster = new Monster();
-                            monster.fromObject(mons);
-                            if (monster.getId() > 200000)
-                            {
-                                baseID = monster.getId() - 200000;
-                                monsters.get(baseID).setAltForm(monster);
-                            } else if (monster.getId() > 100000)
-                            {
-                                baseID = monster.getId() - 100000;
-                                monsters.get(baseID).setAltForm(monster);
-                            } else
-                            {
-                                monsters.add(monster);
-                            }
-                        }
-                        //stringBuilder = new StringBuilder();
-                        //avoid further allocations and just reuse what we have
-                        stringBuilder.delete(0, stringBuilder.length());
-                    } else
-                    {
-                        stringBuilder.append(line);
-                        stringBuilder.append('\n'); //Probably not required, is JSON whitespace sensitive?
-                    }
-                }
-
-                line = reader.readLine();
-            }
-        }
-        catch (IOException exc)
-        {
-            //TODO debug
-            System.out.println("this literally shouldn't happen but it did so i'm stumped.");
-            Monster heh = new Monster();
-            heh.setName("Something pretty bad happened");
-            heh.setId(1);
-            monsters.add(heh);
-            debug = "it blew up";
-            return;
-        }
-        catch (JSONException e)
-        {
-            System.err.println("Malformed JSON file. This shouldn't happen, data is corrupted?");
-            e.printStackTrace();
-            Monster heh = new Monster();
-            heh.setName("Something pretty bad happened");
-            heh.setId(1);
-            monsters.add(heh);
-            return;
-        }
-    }*/
 
     public void FastLoadMonsters(List<Monster> monsters, List<Monster> cleanMonsters)
     {
@@ -173,16 +88,7 @@ public class ElementReader
         AssetManager assetManager = application.getResources().getAssets();
         try (InputStream stream = assetManager.open("pad_na_skills.json"))
         {
-            /*int len = stream.available();
-            byte[] buffer = new byte[len];
-            stream.read(buffer);
-            stream.close();
-
-            System.out.println("Done reading file");
-            String ohcrap = new String(buffer, "UTF-8");*/
-            //JSONTokener tokener = new JSONTokener(ohcrap);
             JsonParser parser = new JsonParser();
-            //JsonArray array = parser.parse(ohcrap).getAsJsonArray();
             JsonElement test = parser.parse(new InputStreamReader(stream));
             System.out.println("Done parsing file");
             JsonArray array = test.getAsJsonArray();
